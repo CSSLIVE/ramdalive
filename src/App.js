c4987bc2273f57854cf201e42e60319697cbcf2c
smartphp <3238355967@qq.com>
2018-12-28 05:53:26 +0800

    **key**
    *** 简短说明干了什么**
     What: 修改了travis文件 . 忘了加点了
    
    ---------------------------------------
    *** 为什么这么做?**
    Why:
    
    ***怎么解决的**
    How:
    
    ---------------------------------------
    
    ``
    
    ---------------------------------------
    
    ````

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>SRC/App.js</code> Hello Travis
+            Edit <code>SRC/App.js</code> Hello  GOOD
           </p>
           <a
             className="App-link"
