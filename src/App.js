f3e856976f9b4df48d5c349a3677ccc2e32e0559
smartphp <3238355967@qq.com>
2018-12-27 12:43:31 +0800

    **key**
    *** 简短说明干了什么**
     What:测试 一下 travis.yml
    
    ---------------------------------------
    *** 为什么这么做?**
    Why:
    
    ***怎么解决的**
    How:
    
    ---------------------------------------
    
    ``
    
    ---------------------------------------
    
    ````

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> Hello Travis
+            Edit <code>SRC/App.js</code> Hello Travis
           </p>
           <a
             className="App-link"
