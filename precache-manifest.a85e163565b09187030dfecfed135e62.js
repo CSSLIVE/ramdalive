407b1731ce47c8fd8594907608945a2c015b893f
smartphp <3238355967@qq.com>
2018-12-27 12:30:53 +0800

    Updates

---
+++ precache-manifest.a85e163565b09187030dfecfed135e62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "53d92d32b3577cf41b31",
+    "url": "/ramdalive/static/css/main.da2d405e.chunk.css"
+  },
+  {
+    "revision": "53d92d32b3577cf41b31",
+    "url": "/ramdalive/static/js/main.53d92d32.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/ramdalive/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "e03205c11a81e142e681",
+    "url": "/ramdalive/static/js/runtime~main.e03205c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ramdalive/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ea11522dafe9663a94a117132e09fc41",
+    "url": "/ramdalive/index.html"
+  }
+];
